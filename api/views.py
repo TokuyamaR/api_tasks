3a06190f1cc84a30717489be51094fe065ec3a74
TokuyamaR <qpflapper.1007@gmail.com>
2021-05-04 11:26:56 +0900

    add view settings

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework import viewets
+from .serializers import TaskSerializer, UserSerializer
+from .models import Task
 
 # Create your views here.
+
+class CreateUserview(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+class MyProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    def get_object(self):
+        return self.requesst.user
+
+class TaskViewSet(viewets.ModelViewset):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
